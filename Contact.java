462745184edae7ccc0fe75e8c5616c4b8dcc61a1
Shuyara Jahan Tusha <97684558+tusha29@users.noreply.github.com>
2023-04-09 20:38:16 +0600

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,17 @@
+public class Contact {
+    private String name;
+    private String phoneNo;
+
+    public Contact(String name, String phoneNo) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+}
